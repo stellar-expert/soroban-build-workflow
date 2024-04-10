6130425027da9d5dfe016bfda0dece5a8f6f6c4b
hawthorne-abendsen <hawthorne@stellar.expert>
2024-04-10 16:49:06 +0300

    switch build process to soroban-cli

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -26,87 +26,27 @@ if [ "${CONTRACT_DIR}" ]; then
     ls -la
 fi
 
+# Print Rust version
+rustc --version
+
+# Print Cargo version
+cargo --version
+
+# Print Soroban version
+soroban --version
+
 # Check if the Cargo.toml file exists
 if [ ! -f "Cargo.toml" ]; then
     echo "ERROR: Cargo.toml file does not exist"
     exit 1
 fi
 
-RUST_VERSION=$(sed -n '/\[package\]/,/^$/{/rust-version = /{s/rust-version = "\(.*\)"/\1/p;}}' Cargo.toml) 
-
-# If rust version is not found in Cargo.toml, set it to the default version
-if [ -z "$RUST_VERSION" ]; then
-    RUST_VERSION="beta"
-fi
-echo "Required Rust version: $RUST_VERSION"
-
-# Installing rust
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y && . $HOME/.cargo/env
-
-# Verify that rustup was installed
-if [ $? -ne 0 ]; then
-    echo "ERROR: Failed to install rustup"
-    exit 1
-fi
-
-# Install the required rust version
-rustup install $RUST_VERSION
-
-# Verify that rust was installed
-if [ $? -ne 0 ]; then
-    echo "ERROR: Failed to install rust"
-    exit 1
-fi
-
-# Set the default rust version
-rustup default $RUST_VERSION
-
-# Verify that rust was installed
-if [ $? -ne 0 ]; then
-    echo "ERROR: Failed to set the default rust version"
-    exit 1
-fi
-
-# Add the cargo bin directory to the PATH
-PATH="/root/.cargo/bin:${PATH}"
-
-# Add the wasm32-unknown-unknown target
-rustup target add wasm32-unknown-unknown
-
-# Verify that wasm32-unknown-unknown target was added
-if [ $? -ne 0 ]; then
-    echo "ERROR: Failed to add wasm32-unknown-unknown target"
-    exit 1
-fi
-
-# Install wasm-opt
-wget https://github.com/WebAssembly/binaryen/releases/download/version_101/binaryen-version_101-x86_64-linux.tar.gz && \
-    tar -xzf binaryen-version_101-x86_64-linux.tar.gz && \
-    cp binaryen-version_101/bin/wasm-opt /usr/local/bin/ && \
-    rm -rf binaryen-version_101 binaryen-version_101-x86_64-linux.tar.gz
-
-# Verify that wasm-opt was installed
-if [ $? -ne 0 ]; then
-    echo "ERROR: Failed to install wasm-opt"
-    exit 1
-fi
-
-echo "Rustc Version:" $(rustc --version) && echo "Cargo Version:" $(cargo --version)
-
-# Build the project with cargo for wasm32-unknown-unknown target
-cargo build --target wasm32-unknown-unknown --release
+# Build the contract
+soroban contract build
 
 # Verify that the build was successful
 if [ $? -ne 0 ]; then
     echo "ERROR: Failed to build the project"
-    exit 1
-fi
-
-echo "Rustc Version:" $(rustc --version) && echo "Cargo Version:" $(cargo --version)
-
-# Check if the Cargo.toml file exists
-if [ ! -f "Cargo.toml" ]; then
-    echo "ERROR: Cargo.toml file does not exist"
     exit 1
 fi
 
@@ -153,7 +93,7 @@ fi
 cd ${MOUNT_DIR}/release
 
 # Optimize the WASM file
-wasm-opt -Oz ${WASM_FILE_NAME} -o ${WASM_FILE_NAME}
+soroban contract optimize --wasm ${WASM_FILE_NAME} --wasm-out ${WASM_FILE_NAME}
 
 # Verify that the optimized.wasm file exists
 if [ ! -f "${MOUNT_DIR}/release/${WASM_FILE_NAME}" ]; then
